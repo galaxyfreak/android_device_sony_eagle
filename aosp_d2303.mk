2f2031d801e91abbba61087daecfcb8085da121a
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-28 15:44:29 +0200

    implement optional prebuilt chromium
    add optional prebuilts/chromium/webview_prebuilt.mk
    
    in case prebuilt is found, it will save build time
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d2303.mk
+++ aosp_d2303.mk
@@ -18,6 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, device/sony/msm8226-common/device.mk)
 $(call inherit-product, vendor/sony/eagle/eagle-vendor.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product-if-exists, prebuilts/chromium/webview_prebuilt.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/eagle/overlay
